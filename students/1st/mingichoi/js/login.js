82a6c60ad869cff61c08221fa6d1c8d4e2344c4c
최민기 <choemingi@choemingiui-MacBook-Pro.local>
2021-06-02 14:24:44 +0900

    Add : 로그인 레이아웃

---
+++ students/1st/mingichoi/js/login.js
@@ -0,0 +1,54 @@
+const inputId = 
+
+
+
+// const inputId = document.getElementsByClassName("userId");
+// const inputPw = document.getElementsByClassName("userPassword");
+// const button = document.getElementsByClassName("loginBtn");
+// const input = document.getElementsByTagName("input")
+
+// function changeColor() {
+//   if(userId.value && userPassword.value) {
+//     loginBtn.removeAttribute('disabled');
+//     loginBtn.style.backgroundColor='#0095F6';
+//   }
+// }
+ 
+//  console.log(inputId);
+//  console.log(inputPw);
+//  console.log(button);
+//  console.log(input);
+ 
+ 
+ // const button = document.getElementsByClassName('loginBtn')[0];
+  // const userId = document.getElementsByClassName("userId")[0];
+  // const userPassword = document.getElementsByClassName("userPassword")[0];
+
+  // // function changeColor() {
+  // //     if(userId.value && userPassword.value) {
+  // //         button.removeAttribute('disabled');
+  // //         button.style.backgroundColor='#0095F6';
+  // //     }
+  // // }
+
+  // // button.addEventListener('keyup',changeColor);
+
+  // console.log(button);
+  // console.log(userId);
+  // console.log(userPassword);
+
+// 구글링해서 찾은 코드
+
+// const button = document.getElementsByClassName('loginBtn')[0];
+// const input = document.getElementsByClassName('input')[0];
+// const name = document.getElementsByClassName("inputName")[0];
+// const pw = document.getElementsByClassName("inputPw")[0];
+
+// function changeColor() {
+//     if(name.value && pw.value) {
+//         button.removeAttribute('disabled');
+//         button.style.backgroundColor='#0095F6';
+//     }
+// }
+
+// input.addEventListener('keyup',changeColor);
