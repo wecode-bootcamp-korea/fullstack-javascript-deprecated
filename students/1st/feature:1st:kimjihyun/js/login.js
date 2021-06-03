2151f885857dd993fae24fe77414bdf89bfe4a38
김지현 <hyun@gimjihyeon-ui-MacBookPro.local>
2021-06-03 22:44:10 +0900

    Modify: 로그인 페이지 추가 구현 사항 수정 반영 진행 중

--- students/1st/feature:1st:kimjihyun/js/login.js
+++ students/1st/feature:1st:kimjihyun/js/login.js
@@ -1,117 +1,52 @@
-const pressId = document.getElementsByClassName('bucks_id')[0];
-const pressPW = document.getElementsByClassName('bucks_password')[0];
-const button = document.querySelector(".bucks_btn"); 
+const pressId = document.getElementsByClassName('ID')[0];
+const pressPW = document.getElementsByClassName('PW')[0];
+const button = document.querySelector('.bucksBtn');
 
-// 1자 이상의 텍스트 입력 시 나타나는 이벤트 구현
-function pressTheKey () {
-    let keyUpID = pressId.value;
-    let keyUpPw = pressPW.value;
+// Id, Pw의 각각의 조건 만족 시 나타나는 이벤트 구현
+function pressKeyMoreThanOne () {
+    let bucksIDValue = pressId.value;
+    let bucksPWValue = pressPW.value;
+    let theInputID = document.querySelector('.ID');
+    let theInputPW = document.querySelector('.PW');
 
-    if (keyUpID.length >= 1 && keyUpPw.length >= 1) {
+    if (bucksIDValue.length >= 1 && bucksIDValue.includes('@')) {
         button.disabled = false;
-        button.style.cursor = "pointer";
-        button.style.backgroundColor = "#1a75ff";
-    } 
-    else {
-        button.disabled = true;
-        button.style.cursor = "default";
+        button.style.cursor = 'pointer';
         button.style.backgroundColor = "#99c2ff";
-    }
-}; 
-
-pressId.addEventListener('keyup', pressTheKey);
-pressPW.addEventListener('keyup', pressTheKey);
-
-
-// border color를 바꾸는 함수 - ID
-function textCheck () {
-    let keyUpID2 = pressId.value;
-    let theInputID = document.querySelector('.bucks_id');
-    console.log(keyUpID2);
-    // let includeTheKey = keyUpID2.includes('@');
-
-    if (keyUpID2.includes('@')) {
         theInputID.style.borderColor = "green";
-    } else {
-        return;
-    };
-}
+    }
 
-pressId.addEventListener('keyup', textCheck);
-
-// border color를 바꾸는 함수 - PW
-function textLengthCheck () {
-    let keyUpPW2 = pressPW.value;
-    let theInputPW = document.querySelector('.bucks_password');
-
-    if (keyUpPW2.length >= 8) {
+    if (bucksIDValue.length >= 1 && bucksIDValue.includes('@') && 
+       (bucksPWValue.length >= 8)) {
+        button.disabled = false;
+        button.style.cursor = 'pointer';
+        button.style.backgroundColor = '#0095F6';
         theInputPW.style.borderColor = "green";
-    } else {
+       }
+    else {
         return;
-    };
-}
+    }
+};
 
-pressPW.addEventListener('keyup', textLengthCheck);
+pressId.addEventListener('input', pressKeyMoreThanOne);
+pressPW.addEventListener('input', pressKeyMoreThanOne);
 
 
 // 초록색으로 변했던 보더가 다시 기본 값으로 돌아오게 하는 함수
-function changeTheColor () {
-    let keyUpID3 = pressId.value;
-    let keyUpPW3 = pressPW.value;
-    let theInputID2 = document.querySelector('.bucks_id');
-    let theInputPW2 = document.querySelector('.bucks_password');
+function returnTheBorderColor () {
+    let bucksIDValue = pressId.value;
+    let bucksPWValue = pressPW.value;
+    let theInputID = document.querySelector('.ID');
+    let theInputPW = document.querySelector('.PW');
 
-    if (!keyUpID3.includes('@') && keyUpPW3.length < 8) {
-        theInputID2.style.borderColor = "rgba(219, 219, 219, 1)";
-        theInputPW2.style.borderColor = "rgba(219, 219, 219, 1)";
+    if (!bucksIDValue.includes('@') && bucksPWValue.length < 8) {
+        theInputID.style.borderColor = "rgba(219, 219, 219, 1)";
+        theInputPW.style.borderColor = "rgba(219, 219, 219, 1)";
     } 
     else {
         return;
     }
 }
 
-pressId.addEventListener('keyup', changeTheColor);
-pressPW.addEventListener('keyup', changeTheColor);
-
-
-
-
-
-
-
- // 중복이 많은 코드
-
-// const myKey = pressId.addEventListener('keyup', function(el) {
-//     let keyUpID = pressId.value;
-//     let keyUpPw = pressPW.value;
-    
-//     if (keyUpID.length >= 1 && keyUpPw.length >= 1) {
-//         button.disabled = false;
-//         button.style.cursor = "pointer";
-//         button.style.backgroundColor = "#1a75ff";
-//     } 
-//     else {
-//         button.disabled = true;
-//         button.style.cursor = "default";
-//         button.style.backgroundColor = "#99c2ff";
-//     }
-// });
-
-// const myKey2 = pressPW.addEventListener('keyup', function(el) {
-//     let keyUpID = pressId.value;
-//     let keyUpPw = pressPW.value;
-    
-//     if (keyUpID.length >= 1 && keyUpPw.length >= 1) {
-//         button.disabled = false;
-//         button.style.cursor = "pointer";
-//         button.style.backgroundColor = "#1a75ff";
-//     } 
-//     else {
-//         button.disabled = true;
-//         button.style.cursor = "default";
-//         button.style.backgroundColor = "#99c2ff";
-//     }
-// });
-
-// function 하나로 만들기. 
-
+pressId.addEventListener('keyup', returnTheBorderColor);
+pressPW.addEventListener('keyup', returnTheBorderColor);
