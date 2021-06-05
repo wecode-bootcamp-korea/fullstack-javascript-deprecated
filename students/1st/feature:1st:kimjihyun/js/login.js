5d77668d5baf2c4c2a277cc65375a5fae707b000
hxyxneee <wlgus3096@gmail.com>
2021-06-05 18:21:01 +0900

    Modify: 스타벅스 로그인 페이지 기본 및 추가 구현 사항 수정

--- students/1st/feature:1st:kimjihyun/js/login.js
+++ students/1st/feature:1st:kimjihyun/js/login.js
@@ -1,52 +1,33 @@
-const pressId = document.getElementsByClassName('ID')[0];
-const pressPW = document.getElementsByClassName('PW')[0];
+const pressId = document.querySelector('#loginID');
+const pressPW = document.querySelector('#loginPW');
 const button = document.querySelector('.bucksBtn');
 
-// Id, Pw의 각각의 조건 만족 시 나타나는 이벤트 구현
-function pressKeyMoreThanOne () {
-    let bucksIDValue = pressId.value;
-    let bucksPWValue = pressPW.value;
-    let theInputID = document.querySelector('.ID');
-    let theInputPW = document.querySelector('.PW');
+// 아이디와 패스워드의 입력 값 체크 함수
+function loginLengthCheck () {
+    const idLength = pressId.value.length;
+    const pwLength = pressPW.value.length;
 
-    if (bucksIDValue.length >= 1 && bucksIDValue.includes('@')) {
-        button.disabled = false;
-        button.style.cursor = 'pointer';
-        button.style.backgroundColor = "#99c2ff";
-        theInputID.style.borderColor = "green";
-    }
-
-    if (bucksIDValue.length >= 1 && bucksIDValue.includes('@') && 
-       (bucksPWValue.length >= 8)) {
-        button.disabled = false;
-        button.style.cursor = 'pointer';
-        button.style.backgroundColor = '#0095F6';
-        theInputPW.style.borderColor = "green";
-       }
-    else {
-        return;
-    }
-};
-
-pressId.addEventListener('input', pressKeyMoreThanOne);
-pressPW.addEventListener('input', pressKeyMoreThanOne);
-
-
-// 초록색으로 변했던 보더가 다시 기본 값으로 돌아오게 하는 함수
-function returnTheBorderColor () {
-    let bucksIDValue = pressId.value;
-    let bucksPWValue = pressPW.value;
-    let theInputID = document.querySelector('.ID');
-    let theInputPW = document.querySelector('.PW');
-
-    if (!bucksIDValue.includes('@') && bucksPWValue.length < 8) {
-        theInputID.style.borderColor = "rgba(219, 219, 219, 1)";
-        theInputPW.style.borderColor = "rgba(219, 219, 219, 1)";
-    } 
-    else {
-        return;
-    }
+    button.disabled = !(idLength && pwLength);
 }
 
-pressId.addEventListener('keyup', returnTheBorderColor);
-pressPW.addEventListener('keyup', returnTheBorderColor);
+// 아이디와 패스워드의 특수 문자 및 입력 값의 길이 체크 함수
+function inputIdCheck () {
+    if (pressId.value.includes('@')) {
+        pressId.classList.add('active');
+    } else {
+        pressId.classList.remove('active');
+    }
+    loginLengthCheck();
+}
+
+function inputPWCheck () {
+    if (pressPW.value.length >= 8) {
+        pressPW.classList.add('active');
+    } else {
+        pressPW.classList.remove('active');
+    }
+    loginLengthCheck();
+}
+
+pressId.addEventListener('input', inputIdCheck);
+pressPW.addEventListener('input', inputPWCheck);
