438c0ce344d551db4b848b3e8a7d14090897e629
undefinedP <kzbaram@gmail.com>
2021-06-08 08:09:47 +0900

    js 기능구현

--- students/1st/MIJEONGPARK/js/login.js
+++ students/1st/MIJEONGPARK/js/login.js
@@ -1,3 +1,18 @@
-ID = document.getElementsByClassName("loginID")
-PW = document.getElementsByClassName("loginPW")
+const inputID = document.getElementsByClassName('loginID')[0]
+const inputPW = document.getElementsByClassName('loginPW')[0]
+const loginBtn = document.getElementsByClassName('btn')[0]
 
+const activeBtn = document.getElementsByClassName('active')[0]
+
+
+function handleInput() {
+  if (inputID.value.length > 0 && inputPW.value.length > 0) {
+    loginBtn.classList.add('active'); 
+  } else {
+    loginBtn.classList.remove('active');
+  }
+}
+
+
+inputID.addEventListener('input', handleInput);
+inputPW.addEventListener('input', handleInput);
