74329bcb70a2c38538a32a0c6607b885463ec6cb
hxyxneee <wlgus3096@gmail.com>
2021-06-08 20:41:14 +0900

    Refactor: 아이디값만 입력 시 경고창이 뜨도록 수정

--- students/1st/feature:1st:kimjihyun/js/detail.js
+++ students/1st/feature:1st:kimjihyun/js/detail.js
@@ -12,22 +12,22 @@ const inputID = document.querySelector('.reviewIDForm');
 const addReviewSpace = document.querySelector('.textAddContainer');
 const addIDSpace = document.querySelector('.textAddID');
 
-// 엔터 시 input 내의 텍스트가 등록되는 기능
-function makeIDInput () {
-    const inputID = reviewIDContainer.value;
-    addIDSpace.textContent = inputID;
+// 엔터 시 input 내의 텍스트가 등록되는 기능 - id만 입력되고, review가 없으면 경고창이 뜬다
+function makeIDInput (el) {
+    el.preventDefault();
+    const inputTextMessage = reviewContainer.value;
+    if (!inputTextMessage) {
+        alert('리뷰를 입력해주세요!');
+    }
 }
+inputID.addEventListener('submit', makeIDInput);
 
 function makereviewInput (el) {
     el.preventDefault();
-    const inputText = reviewIDContainer.value;
+    const inputText = reviewContainer.value;
+    const inputTextID = reviewIDContainer.value;
+    addIDSpace.textContent = inputTextID;
     addReviewSpace.textContent = inputText;
-    makeIDInput();
 }
 
 inputReview.addEventListener('submit', makereviewInput);
-
-
-
-
-
