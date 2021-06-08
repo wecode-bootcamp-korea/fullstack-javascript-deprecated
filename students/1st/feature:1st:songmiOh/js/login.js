4d657451b0ee4ef5c2e36926651281618ae69932
songmiO <dhthd1060@gmail.com>
2021-06-08 16:19:30 +0900

    Add : 필수 로그인 기능 구현 완료

--- students/1st/feature:1st:songmiOh/js/login.js
+++ students/1st/feature:1st:songmiOh/js/login.js
@@ -5,8 +5,10 @@ const loginBt = document.getElementsByClassName("loginBt")[0];
 function changeButton() {
   if(loginId.value.length > 0 && loginPw.value.length > 0) {
     loginBt.style.backgroundColor = "rgb(0, 0, 250)";
+    loginBt.disabled = false;
   } else {  
     loginBt.style.backgroundColor = "rgb(135, 206, 250)";
+    loginBt.disabled = true;
   }
 }
 
